50003e9054fd8426184a67b68394536ea010cbe1
Shivam Kumar <svm.singh.01@gmail.com>
2025-04-06 18:39:33 +0530

    fixed routes

--- PrimeBasket/djangojwt/shop/urls.py
+++ PrimeBasket/djangojwt/shop/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from .views import get_products, search_products, add_to_cart, remove_from_cart, update_cart_quantity
+from .views import (
+    get_products,
+    search_products,
+    get_single_product,
+    add_to_cart,
+    remove_from_cart,
+    update_cart_quantity,
+)
 
 urlpatterns = [
-    path('products/', get_products),
-    path('search/', search_products),
-    path('cart/add/', add_to_cart),
-    path('cart/remove/', remove_from_cart),
-    path('cart/update/', update_cart_quantity),
+    path("products/", get_products),
+    path("products/<int:product_id>/", get_single_product),
+    path("search/", search_products),
+    path("cart/add/", add_to_cart),
+    path("cart/remove/", remove_from_cart),
+    path("cart/update/", update_cart_quantity),
 ]
