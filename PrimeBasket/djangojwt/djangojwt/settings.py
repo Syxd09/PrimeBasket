14f245198877b40231c0f1141a87c897c3399d8c
Shivam Kumar <svm.singh.01@gmail.com>
2025-04-06 19:00:27 +0530

    Fix CORS and enforce HTTPS

--- PrimeBasket/djangojwt/djangojwt/settings.py
+++ PrimeBasket/djangojwt/djangojwt/settings.py
@@ -51,12 +51,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 REST_FRAMEWORK = {
@@ -167,3 +167,16 @@ LOGGING = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "https://primebasket-store.onrender.com",  # Your frontend URL
+]
+
+CORS_ALLOW_CREDENTIALS = True  # If you need cookies/authentication
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://primebasket-store.onrender.com",  # Allow CSRF requests from frontend
+]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
